bc331b741ea249d7dadf20a0deabdba494ad76b3
Sa_Sek <sasek2019@gmail.com>
2023-11-09 10:49:35 +0700

    init country catalog

---
+++ src/components/CountryCatalog.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import { ref } from "vue";
+    const count = ref(0);
+    async function increase() {
+        count.value++
+    }
+</script>
+
+<template>
+    <button @click="increase">{{ count }}</button>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+//import TheWelcome from '@/components/TheWelcome.vue'
+import CountryCatalog from "@/components/CountryCatalog.vue";
+</script>
+
+<template>
+  <main>
+    <!--<TheWelcome />-->
+    <CountryCatalog />
+  </main>
+</template>
